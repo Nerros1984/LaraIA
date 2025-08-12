8c0a801414d0a3734f1ca17e6d39ea3e3a1ef426
Nerros1984 <javierhernandezav@gmail.com>
2025-08-12 13:47:35 +0200

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,10 +1,9 @@
-// api/orders.js
 import Stripe from "stripe";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   const orderId = "ord_" + Date.now();
 
   const session = await stripe.checkout.sessions.create({
@@ -12,7 +11,7 @@ export default async function handler(req, res) {
     line_items: [{ price: process.env.STRIPE_PRICE_ID, quantity: 1 }],
     success_url: `https://laraia.es/blog.html?paid=1&order_id=${orderId}`,
     cancel_url: `https://laraia.es/blog.html`,
-    client_reference_id: orderId,
+    client_reference_id: orderId
   });
 
   res.writeHead(303, { Location: session.url });
